941e936ab64482beecc980fddd95970133c68a61
Qingping He <qphe95@gmail.com>
2026-06-18 19:23:34 -0700

    fix

--- browser-emulator/src/jobs.c
+++
@@ -1,8 +0,0 @@
-#include "jobs.h"
-
-bool jobs_start_thread(pthread_t *thread, void *(*fn)(void *), void *arg) {
-    if (!thread || !fn) {
-        return false;
-    }
-    return pthread_create(thread, NULL, fn, arg) == 0;
-}
